993292776d6a925bd1ee20f001c48c2b9dce503c
YoelRibero <yoelribero9@gmail.com>
2021-06-02 20:38:26 -0300

    Add 30 questions for the quizz app

--- src/utils/db.js
+++ src/utils/db.js
@@ -344,6 +344,425 @@ export const db = {
           isCorrect: true
         }
       ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Los signos de pare y ceda el paso son:',
+      options: [
+        {
+          idOption: '1-12',
+          value: 'Señales verticales reglamentarias que establecen prohibiciones o reestricciones',
+          isCorrect: true
+        },
+        {
+          idOption: '2-12',
+          value: 'Señales de advertencia',
+          isCorrect: false
+        },
+        {
+          idOption: '3-12',
+          value: 'Dispositivo de información',
+          isCorrect: false
+        },
+        {
+          idOption: '4-12',
+          value: 'Solo b) y c) son correctas',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'La luz amarilla(ámbar)contínua en un semáforo le indica que:',
+      options: [
+        {
+          idOption: '1-13',
+          value: 'Si comenzó el cruce está obligado a despejarla',
+          isCorrect: false
+        },
+        {
+          idOption: '2-13',
+          value: 'Si no comenzó el cruce, le da derecho a cruzar',
+          isCorrect: false
+        },
+        {
+          idOption: '3-13',
+          value: 'Tengo precaución y no ingreso al cruce',
+          isCorrect: false
+        },
+        {
+          idOption: '4-13',
+          value: 'Solo a) y c) son correctas',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Esta prohibído estacionar a menos de 10mts antes de un cartel de pare o ceda paso',
+      options: [
+        {
+          idOption: '1-14',
+          value: 'Verdadero',
+          isCorrect: true
+        },
+        {
+          idOption: '2-14',
+          value: 'Falso',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Cuando enfrenta un cartel de ceda el paso, usted debe:',
+      options: [
+        {
+          idOption: '1-15',
+          value: 'Reducir la velocidad y detenerse si fuera necesario',
+          isCorrect: false
+        },
+        {
+          idOption: '2-15',
+          value: 'Dejar pasar a los vehículos procedentes de la calle transversal',
+          isCorrect: false
+        },
+        {
+          idOption: '3-15',
+          value: 'Preferencia a los peatones',
+          isCorrect: false
+        },
+        {
+          idOption: '4-15',
+          value: 'Todas son correctas',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'En ruta,usted no podrá adelantar a otro vehículo usando la mitad izquierda de la calzada',
+      options: [
+        {
+          idOption: '1-16',
+          value: 'En un repecho próximo a una cumbre',
+          isCorrect: false
+        },
+        {
+          idOption: '2-16',
+          value: 'En puentes o túneles',
+          isCorrect: false
+        },
+        {
+          idOption: '3-16',
+          value: 'Cuando existe una línea amarilla contínua',
+          isCorrect: false
+        },
+        {
+          idOption: '4-16',
+          value: 'Todas son correctas',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'El conductor del vehículo que circula delante suyo saca el brazo fuera del mismo y lo extiede hacia arriba,significa que realizará:',
+      options: [
+        {
+          idOption: '1-17',
+          value: 'Un giro a la derecha',
+          isCorrect: true
+        },
+        {
+          idOption: '2-17',
+          value: 'Un giro a la izquierda',
+          isCorrect: false
+        },
+        {
+          idOption: '3-17',
+          value: 'Una detención',
+          isCorrect: false
+        },
+        {
+          idOption: '4-17',
+          value: 'Marcha atrás',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'El hidroplaneamiento es un fenómeno que se produce cuando:',
+      options: [
+        {
+          idOption: '1-18',
+          value: 'Circula a exceso de velocidad y acciona el freno de mano',
+          isCorrect: false
+        },
+        {
+          idOption: '2-18',
+          value: 'El dibujo de los neumáticos no puede desalojar el agua de la calzada',
+          isCorrect: true
+        },
+        {
+          idOption: '3-18',
+          value: 'Frena en forma imprevista sobre una mancha de aceite',
+          isCorrect: false
+        },
+        {
+          idOption: '4-18',
+          value: 'Gira a alta velocidad',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'La hipoglucemia compromete la capacidad para conducir',
+      options: [
+        {
+          idOption: '1-19',
+          value: 'Falso',
+          isCorrect: false
+        },
+        {
+          idOption: '2-19',
+          value: 'Verdadero',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Se produce hidroplaneamiento cuando se forma una película de agua entre la calzada y el neumático,haciendo que se pierda el control del vehículo',
+      options: [
+        {
+          idOption: '1-20',
+          value: 'Verdadero',
+          isCorrect: true
+        },
+        {
+          idOption: '2-20',
+          value: 'Falso',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Los medicamentos pueden disminuir el rendimiento psicomotor de la persona',
+      options: [
+        {
+          idOption: '1-21',
+          value: 'Falso',
+          isCorrect: false
+        },
+        {
+          idOption: '2-21',
+          value: 'Verdadero',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'El velocímetro permite:',
+      options: [
+        {
+          idOption: '1-22',
+          value: 'Controlar la velocidad',
+          isCorrect: false
+        },
+        {
+          idOption: '2-22',
+          value: 'No pasarnos de la velocidad permitida',
+          isCorrect: false
+        },
+        {
+          idOption: '3-22',
+          value: 'Solo a) es correcta',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Todo vehículo que se encuentra circulando una rotonda tiene preferencia sobre quién pretende ingresar en ella',
+      options: [
+        {
+          idOption: '1-23',
+          value: 'Verdadero',
+          isCorrect: true
+        },
+        {
+          idOption: '2-23',
+          value: 'Falso',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Pueden circular por el carril preferencial de buses:',
+      options: [
+        {
+          idOption: '1-24',
+          value: 'Motocicletas y vehículos particulares,con mas de 3 pasajeros',
+          isCorrect: false
+        },
+        {
+          idOption: '2-24',
+          value: 'Excepcionalmente en caso de emergencia vehículos del ministerio',
+          isCorrect: false
+        },
+        {
+          idOption: '3-24',
+          value: 'Buses del transporte colectivo de pasajeros',
+          isCorrect: false
+        },
+        {
+          idOption: '4-24',
+          value: 'Solo b) y c) con correctas',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'El alcohol es compatible con la conducción de vehículos',
+      options: [
+        {
+          idOption: '1-25',
+          value: 'Verdadero',
+          isCorrect: false
+        },
+        {
+          idOption: '2-25',
+          value: 'Falso',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Al momento de ser adelantado por otro vehículo, usted:',
+      options: [
+        {
+          idOption: '1-26',
+          value: 'Mantiene o disminuye la velocidad',
+          isCorrect: true
+        },
+        {
+          idOption: '2-26',
+          value: 'Aumenta la velocidad',
+          isCorrect: false
+        },
+        {
+          idOption: '3-26',
+          value: 'Enciende el señalero derecho para dar paso a la maniobra',
+          isCorrect: false
+        },
+        {
+          idOption: '4-26',
+          value: 'Se acerca al carril izquierdo',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: '¿Podemos adelantar en un repecho?',
+      options: [
+        {
+          idOption: '1-27',
+          value: 'No',
+          isCorrect: true
+        },
+        {
+          idOption: '2-27',
+          value: 'Solo con extremo cuidado',
+          isCorrect: false
+        },
+        {
+          idOption: '3-27',
+          value: 'Solo en calles de un único sentido',
+          isCorrect: false
+        },
+        {
+          idOption: '4-27',
+          value: 'Se puede siempre',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'En carretera se debe aminorar la marcha en:',
+      options: [
+        {
+          idOption: '1-28',
+          value: 'Zonas urbanas y caseríos',
+          isCorrect: false
+        },
+        {
+          idOption: '2-28',
+          value: 'Curvas y cruces',
+          isCorrect: false
+        },
+        {
+          idOption: '3-28',
+          value: 'Pasos a nivel y puentes',
+          isCorrect: false
+        },
+        {
+          idOption: '4-28',
+          value: 'Todas las anteriores son correctas',
+          isCorrect: true
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'El tiempo de reacción del conductor se ve afectado por:',
+      options: [
+        {
+          idOption: '1-29',
+          value: 'El estado de la ruta',
+          isCorrect: false
+        },
+        {
+          idOption: '2-29',
+          value: 'La velocidad a la cual conduce',
+          isCorrect: false
+        },
+        {
+          idOption: '3-29',
+          value: 'El cansancio',
+          isCorrect: true
+        },
+        {
+          idOption: '4-29',
+          value: 'Los defectos mecánicos del vehículo',
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      id: uuid(),
+      quiz: 'Usted puede circular sin espejo retrovisor cuando circula a baja velocidad',
+      options: [
+        {
+          idOption: '1-30',
+          value: 'Verdadero',
+          isCorrect: true
+        },
+        {
+          idOption: '2-30',
+          value: 'False',
+          isCorrect: true
+        }
+      ]
     }
   ]
 }
